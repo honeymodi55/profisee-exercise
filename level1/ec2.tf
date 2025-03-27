089b40b48f31167308cb2fc4a6d86284cd894d40
honeymodi55 <honeymodi1997@gmail.com>
2025-03-27 19:35:08 -0400

    triggering a change

--- level1/ec2.tf
+++ level1/ec2.tf
@@ -26,6 +26,15 @@ resource "aws_security_group" "profiseeDemoEC2-sg" {
             "0.0.0.0/0"
         ]
     }
+    ingress {
+        description = "https"
+        from_port = "80"
+        to_port = "80"
+        protocol = "tcp"
+        cidr_blocks = [
+            "0.0.0.0/0"
+        ]
+    }
     egress {
         from_port = "0"
         to_port = "0"
